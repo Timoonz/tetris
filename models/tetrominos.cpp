f945998b20f5d513de6da2756e06e0802463669c
Timoonz <timotheeeheraud@gmail.com>
2025-11-19 19:41:52 +0100

    initial commit

---
+++ models/tetrominos.cpp
@@ -0,0 +1,3 @@
+#include "tetrominos.h"
+
+tetrominos::tetrominos() {}
---
+++ models/tetrominos.h
@@ -0,0 +1,10 @@
+#ifndef TETROMINOS_H
+#define TETROMINOS_H
+
+class tetrominos
+{
+public:
+    tetrominos();
+};
+
+#endif // TETROMINOS_H
